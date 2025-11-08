03bcda26e5fc32718a18105ef0dd688a49881220
LoriTechMoms <lahgubler@gmail.com>
2025-11-07 21:12:30 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// Toggle dropdown when button clicked
-document.querySelector('.dropbtn').addEventListener('click', function () {
-    document.querySelector('.dropdown-content').classList.toggle('show');
-});
-
-// Close dropdown if clicked outside
-window.addEventListener('click', function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        const dropdowns = document.querySelectorAll('.dropdown-content');
-        dropdowns.forEach(dropdown => {
-            if (dropdown.classList.contains('show')) {
-                dropdown.classList.remove('show');
-            }
-        });
-    }
-});
+// 
